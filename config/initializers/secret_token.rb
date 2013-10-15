971e06225ffe955eb8041bccfb76e0112e32f806
Jason Ayre <jason.ayre@moneydesktop.com>
2013-10-15 13:04:15 -0600

    dcell bug

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DcellBugExample::Application.config.secret_key_base = '8cd6e3db333c44f5887bd8f342f665a2567a528b8cc30052d913ce773c119370c82b0b32d90193e7766b54f4a3078134aafc565df53c8aa942aebd7262ce9001'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DcellBugExample::Application.config.session_store :cookie_store, key: '_dcell_bug_example_session'
